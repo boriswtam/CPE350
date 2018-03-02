d9ea0224a0ed66d55b4a340fcfdf2c9b728a52e9
zephnord <zeph.nord@gmail.com>
2018-03-02 08:58:41 -0800

    Clear rpm_sensor warnings

--- Sensor_Code/labjack_code/rpm_sensor.c
+++ Sensor_Code/labjack_code/rpm_sensor.c
@@ -1,3 +1,5 @@
+#include <LabJackM.h>
+#include "LJM_Utilities.h"
 #include "rpm_sensor.h"
 #include "sys/time.h"
 #include "time.h"
@@ -5,8 +7,8 @@
 #define HIGH 2.5 // switching voltage
 
 void rpm_sensor_init(int handle, struct rpm_sensor_struct * rpm_sensor) {
-	clock_t start_t = 0, end_t = 0, total_t = 0;
-	int interrupts = 0;
+	clock_t start_t, end_t, total_t = 0;
+	int rotations = 0;
 
 	int err;
 	double value = 0;
@@ -15,11 +17,17 @@ void rpm_sensor_init(int handle, struct rpm_sensor_struct * rpm_sensor) {
 	// Read AIN from the LabJack
 	err = LJM_eReadName(handle, NAME, &value);
 
-	if (value > HIGH) {
+	ErrorCheck(err, "LJM_eReadName");
 
+	while ((total_t/CLOCKS_PER_SEC) < 1) {
+		if (value > HIGH) {
+			start_t = clock();
+			rotations++;
+		}
+		else {
+			end_t = clock();
+		}
 	}
 
-	start_t = clock();
-	
-	end_t = clock();
+	total_t = end_t - start_t;
 }
