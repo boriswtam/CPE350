b9f645a65504643fb61a7a4b06f40afb9a6dd0ee
tjgesell <32271791+tjgesell@users.noreply.github.com>
2018-03-05 08:17:15 -0800

    Add files via upload
    Code for running the motor

---
+++ runMotor.c
@@ -0,0 +1,39 @@
+/**
+ * Name: single_dio_write.c
+ * Desc: Demonstrates how to set a single digital state on a LabJack.
+**/
+
+// For printf
+#include <stdio.h>
+
+// For the LabJackM Library
+#include <LabJackM.h>
+
+// For LabJackM helper functions, such as OpenOrDie, PrintDeviceInfoFromHandle,
+// ErrorCheck, etc.
+#include "../../LJM_Utilities.h"
+#include "../../LJM_StreamUtilities.h"
+
+int runMotor()
+{
+	int handle;
+
+	// Open first found LabJack
+	handle = OpenOrDie(LJM_dtANY, LJM_ctANY, "LJM_idANY");
+	// handle = OpenSOrDie("LJM_dtANY", "LJM_ctANY", "LJM_idANY");
+
+	PrintDeviceInfoFromHandle(handle);
+
+                 
+        EnableFIO0PulseOut(handle, 400, 2147483647, 15000);
+        sleep(2);
+        EnableFIO0PulseOut(handle, 400, 2147483647, 5000);
+        sleep(2);
+        EnableFIO0PulseOut(handle, 400, 2147483647, 20000);
+  
+	CloseOrDie(handle);
+
+	WaitForUserIfWindows();
+
+	return LJME_NOERROR;
+}
