c14cd1b57ba8ea7553f8fa1f3f15c0ef8e5eda4f
Trevor Gesell <tgesell@calpoly.edu>
2018-03-05 21:02:49 -0800

    This should be the rest of the sensor code

--- Sensor_Code/labjack_code/current_sensor.c
+++ Sensor_Code/labjack_code/current_sensor.c
@@ -6,21 +6,33 @@
 #include <LabJackM.h>
 #include "LJM_Utilities.h"
 #include "current_sensor.h"
+#include "sys/time.h"
+#include "time.h"
+
 
 void current_sensor_init(int handle, struct current_sensor_struct * current_sensor)
 {
-	int err;
+        clock_t start_t, total_t = 0;
 
 	// Set up for reading AIN value
 	double value = 0;
 	const char * NAME = "AIN1";
 
-	// Read AIN from the LabJack
-	err = LJM_eReadName(handle, NAME, &value);
-	
-	// Process the voltage to current
-	double i = (value - (2.5)) / (0.028);
-	current_sensor->current = i;
+	double max_value = -100;
+	double min_value = 100;
 
-	ErrorCheck(err, "LJM_eReadName");
+	// Read AIN from the LabJack
+        start_t = clock();
+        while ((total_t/(CLOCKS_PER_SEC / 1)) < 1) {
+                LJM_eReadName(handle, NAME, &value);
+                if (value > max_value) {
+			max_value = value;
+                }
+                else if (value < min_value) {
+                        min_value = value;
+                }
+                total_t = clock() - start_t;
+        }
+
+	current_sensor->current = max_value - min_value;
 }
--- Sensor_Code/labjack_code/run_motor.c
+++ Sensor_Code/labjack_code/run_motor.c
@@ -22,39 +22,29 @@ void EnableDIO15PulseOut(int handle, int pulseRate, int numPulses, int config)
 
 	printf("Enabling pulses on FIO0 at a %d Hz pulse rate\n", pulseRate);
 
-	WriteNameOrDie(handle, "DIO15_EF_ENABLE", 0);
+	WriteNameOrDie(handle, "DIO0_EF_ENABLE", 0);
 	WriteNameOrDie(handle, "DIO_EF_CLOCK0_DIVISOR", 8);
 	WriteNameOrDie(handle, "DIO_EF_CLOCK0_ROLL_VALUE", rollValue);
 	WriteNameOrDie(handle, "DIO_EF_CLOCK0_ENABLE", 1);
-	WriteNameOrDie(handle, "DIO15_EF_INDEX", 2);
-	WriteNameOrDie(handle, "DIO15_EF_OPTIONS", 0);
-	WriteNameOrDie(handle, "DIO15", 0);
-	WriteNameOrDie(handle, "DIO15_EF_CONFIG_A", config);
-	WriteNameOrDie(handle, "DIO15_EF_CONFIG_B", 0);
-	WriteNameOrDie(handle, "DIO15_EF_CONFIG_C", numPulses);
-	WriteNameOrDie(handle, "DIO15_EF_ENABLE", 1);
+	WriteNameOrDie(handle, "DIO0_EF_INDEX", 2);
+	WriteNameOrDie(handle, "DIO0_EF_OPTIONS", 0);
+	WriteNameOrDie(handle, "DIO0", 0);
+	WriteNameOrDie(handle, "DIO0_EF_CONFIG_A", config);
+	WriteNameOrDie(handle, "DIO0_EF_CONFIG_B", 0);
+	WriteNameOrDie(handle, "DIO0_EF_CONFIG_C", numPulses);
+	WriteNameOrDie(handle, "DIO0_EF_ENABLE", 1);
 }
 
-int run_motor()
+int run_motor(int handle)
 {
-	int handle;
-
-	// Open first found LabJack
-	handle = OpenOrDie(LJM_dtANY, LJM_ctANY, "LJM_idANY");
-	// handle = OpenSOrDie("LJM_dtANY", "LJM_ctANY", "LJM_idANY");
-
 	PrintDeviceInfoFromHandle(handle);
 
                  
         EnableDIO15PulseOut(handle, 400, 2147483647, 15000);
-        sleep(2);
+        sleep(1);
         EnableDIO15PulseOut(handle, 400, 2147483647, 5000);
-        sleep(2);
+        sleep(1);
         EnableDIO15PulseOut(handle, 400, 2147483647, 20000);
   
-	CloseOrDie(handle);
-
-	WaitForUserIfWindows();
-
 	return LJME_NOERROR;
 }
