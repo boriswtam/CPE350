0ae0e4a78b596ce2ab8be46edce536ff035105a6
Trevor Gesell <tgesell@calpoly.edu>
2018-03-11 17:48:05 -0700

    First good data

--- Sensor_Code/labjack_code/main_all_sensors.c
+++ Sensor_Code/labjack_code/main_all_sensors.c
@@ -65,23 +65,21 @@ int main(int argc, char * argv[]) {
 	current_sensor.handle = handle;
 	rpm_sensor.handle = handle;
 
-	FILE *file = fopen(file_name, "a+");
-//	fprintf(file, "%s\n", "tmp local (C), tmp object (C), sound voltage (V), current (A),");
-        fflush(file);
-
-//Code to run the motor
         rms.supply_voltage = 12;
         rms.frequency = 50;
         rms.duty_cycle = .07;
-	run_motor(handle, &rms);
+        run_motor(handle, &rms);
         sleep(1);
         rms.duty_cycle = .025;
         run_motor(handle, &rms);
         sleep(1);
         rms.duty_cycle = .07;
         run_motor(handle, &rms);
-////////////////////////////////
+      //  init_motor(handle, rms);
 
+
+	FILE *file = fopen(file_name, "a+");
+        fflush(file);
 
  	tv.tv_sec = 0;
 	tv.tv_usec = 0;
@@ -92,6 +90,7 @@ int main(int argc, char * argv[]) {
 	pthread_create(&my_thread, NULL, (void *)&rpm_sensor_init, (void *)(&rpm_sensor));
 
 	while(1) {
+		//tmp local (C), tmp object (C), sound voltage (V), current (A), rpm, supply voltage (V), frequency (Hz), Duty Cycle, Error
 		fprintf(file, "%.2f%c%.2f%c%.2lf%c%.4lf%c%d%c%d%c%d%c%.4f%c%d\n", tmp007_sensor.local_tmp, ',', tmp007_sensor.object_tmp, ',',
 			 sound_detector.voltage, ',', current_sensor.current, ',', rpm_sensor.rpm, ',', rms.supply_voltage, ',', rms.frequency, ',', rms.duty_cycle, ',', error);
 		fflush(file);
@@ -113,3 +112,4 @@ int main(int argc, char * argv[]) {
 	WaitForUserIfWindows();
 	return LJME_NOERROR;
 }
+
